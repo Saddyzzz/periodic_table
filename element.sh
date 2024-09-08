a11e44d33abd61c1719545f1b18f32b57e225330
Saddyzzz Ng <pratk.jr@gmail.com>
2024-09-08 16:32:34 +0000

    feat: Added comments to element.sh

--- element.sh
+++ element.sh
@@ -2,21 +2,35 @@
 #!/bin/bash
 
 
-# Adding a comment to trigger a commit
+#!/bin/bash
+
+# PSQL variable for querying the database
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-
+# Check if an argument is provided
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
-  exit
+  exit 0
 fi
 
+# Check if the argument is a number (atomic number)
 if [[ $1 =~ ^[0-9]+$ ]]
 then
-  ELEMENT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements JOIN properties ON elements.atomic_number = properties.atomic_number JOIN types ON properties.type_id = types.type_id WHERE elements.atomic_number = $1")
-elif [[ $1 =~ ^[A-Za-z]{1,2}$ ]]
-then
-  ELEMENT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements JOIN properties ON elements.atomic_number = properties.atomic_number JOIN types ON properties.type_id = types.type_id WHERE elements.symbol = '$1'")
+  ELEMENT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number=$1")
 else
-  ELEMENT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting
+  # Check if the argument is a symbol or a name
+  ELEMENT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.symbol='$1' OR elements.name='$1'")
+fi
+
+# If the element is not found
+if [[ -z $ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+else
+  # Parse the element information
+  IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT"
+  
+  # Output the element information
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+fi
